0594f52ff28d291a1cb2b98b5293453d9dc870ba
webloupe <info@webloupe.de>
2021-12-15 10:26:56 +0100

    cleanup

---
+++ WeloProductCategory.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace WeloProductCategory;
+
+use Exception;
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class WeloProductCategory
+ *
+ * @author    Cyprien Nkeneng <cyprien.nkeneng@webloupe.de> - www.webloupe.de
+ * @copyright Copyright (c) 2017-2021 WEB LOUPE
+ * @package   WeloProductCategory
+ * @link      https://www.webloupe.de
+ * @version   1
+ */
+class WeloProductCategory extends Plugin
+{
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->setParameter('welo_product_category.plugin_dir', $this->getPath());
+        $container->setParameter('welo_product_category.namespace', $this->getName());
+
+        parent::build($container);
+    }
+
+    /**
+     * @param InstallContext $context
+     *
+     * @return void
+     */
+    public function install(InstallContext $context)
+    {
+        if (false === $context->assertMinimumVersion(5.2)) {
+            $context->scheduleMessage("Failed: Plugin required minimal version of Shopware 5.3 !");
+            return;
+        }
+
+        try {
+            $context->scheduleMessage("Plugin successfully installed");
+        } catch (Exception $e) {
+            $context->scheduleMessage('Error: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * @param UninstallContext $context
+     *
+     * @return void
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        try {
+            $context->scheduleClearCache(UninstallContext::CACHE_LIST_DEFAULT);
+            $context->scheduleMessage("Plugin successfully uninstalled");
+        } catch (Exception $e) {
+            $context->scheduleMessage("Uninstall error: " . $e->getMessage());
+        }
+    }
+}
